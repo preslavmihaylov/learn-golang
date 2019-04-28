c447dde99a2bee99101a46cdb9095e0b2436284c
PreslavMihaylov <preslav@pmihaylov.com>
2019-04-28 11:19:47 +0300

    added exercise 3 from ninja level 1

---
+++ ninja-level-01/ex03/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var x = 42
+var y = "James Bond"
+var z = true
+
+func main() {
+	// the assigned values are called "zero values"
+	s := fmt.Sprintf("%v %v %v", x, y, z)
+	fmt.Println(s)
+}
