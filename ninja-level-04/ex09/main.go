4f44b8aba24932126b2ab293936d2a25877de9a1
PreslavMihaylov <preslav@pmihaylov.com>
2019-04-29 08:43:36 +0300

    added exercise 09 of ninja level 4

---
+++ ninja-level-04/ex09/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	mp := map[string][]string{
+		"Bond James":      {"Shaken, not stirred", "Martinis", "Women"},
+		"Moneypenny Miss": {"James Bond", "Literature", "Computer Science"},
+		"No Doctor":       {"Being Evil", "Ice Cream", "Sunsets"},
+	}
+
+	mp["Mcleod Todd"] = []string{"Go", "Talking", "Teaching"}
+
+	for k, v := range mp {
+		fmt.Println(k)
+		for i, v1 := range v {
+			fmt.Println("\t", i, v1)
+		}
+	}
+}
