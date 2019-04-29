1eff14668a13a5be7ba86f949028950762a4611b
PreslavMihaylov <preslav@pmihaylov.com>
2019-04-29 08:00:50 +0300

    added exercise 02 of ninja level 4

---
+++ ninja-level-04/ex02/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := make([]int, 10)
+	arr[0] = 10
+	arr[1] = 20
+	arr[2] = 30
+	arr[3] = 40
+	arr[4] = 50
+
+	for _, v := range arr {
+		fmt.Println(v)
+	}
+
+	fmt.Printf("%T\n", arr)
+}
