48322410ce0bf121d7e264f8bedaf46bd6b9a252
PreslavMihaylov <preslav@pmihaylov.com>
2019-05-03 12:15:41 +0300

    added exercise 04 of ninja level 10

---
+++ ninja-level-10/ex04/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := gen()
+
+	receive(c)
+
+	fmt.Println("about to exit")
+}
+
+func gen() <-chan int {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+
+		close(c)
+	}()
+
+	return c
+}
+
+func receive(c <-chan int) {
+	for {
+		select {
+		case v, ok := <-c:
+			if !ok {
+				fmt.Println("Quitting")
+				return
+			}
+
+			fmt.Println(v)
+		}
+	}
+}
