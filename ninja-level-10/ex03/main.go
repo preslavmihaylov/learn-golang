6bead9d9382c39360a2182068b248bd6ed1bcc6a
PreslavMihaylov <preslav@pmihaylov.com>
2019-05-03 11:58:58 +0300

    added exercise 03 of ninja level 10

---
+++ ninja-level-10/ex03/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := gen()
+	receive(c)
+
+	fmt.Println("about to exit")
+}
+
+func gen() <-chan int {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+
+		close(c)
+	}()
+
+	return c
+}
+
+func receive(c <-chan int) {
+	for v := range c {
+		fmt.Println(v)
+	}
+}
