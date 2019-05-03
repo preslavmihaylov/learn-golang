bcc75bbe754ad239102ae7280d851e4cf2391eb8
PreslavMihaylov <preslav@pmihaylov.com>
2019-05-03 11:54:59 +0300

    added exercise 02 of ninja level 10

---
+++ ninja-level-10/ex02/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cs := make(chan int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
+}
