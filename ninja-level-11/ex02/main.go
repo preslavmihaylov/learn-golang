1b907d4bb865634e30d3c027f03e049c59c281f2
PreslavMihaylov <preslav@pmihaylov.com>
2019-05-04 18:54:40 +0300

    added exercise 02 of ninja level 11

---
+++ ninja-level-11/ex02/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := toJSON(p1)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	fmt.Println(string(bs))
+
+}
+
+// toJSON needs to return an error also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	if err != nil {
+		err = fmt.Errorf("json.Marshal threw err: %s", err)
+	}
+
+	return bs, err
+}
