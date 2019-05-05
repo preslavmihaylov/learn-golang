e47741daec316f8a598fc040f66711fa90a5869b
PreslavMihaylov <preslav@pmihaylov.com>
2019-05-05 14:00:55 +0300

    added exercise 04 of ninja level 11

---
+++ ninja-level-11/ex04/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
+}
+
+func main() {
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		return 0, sqrtError{"10", "20", errors.New("Sqrt Error")}
+	}
+	return 42, nil
+}
