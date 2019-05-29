39fe1f0f300bd5fa815872e43be1ac92990221cc
PreslavMihaylov <preslav@pmihaylov.com>
2019-05-29 08:57:21 +0300

    decoupled cmdline options from cyoa handler. Accepting them as factory method dependencies now

---
+++ gophercises/ex03-cyoa/cmd/web/cyoa_handlers/handler.go
@@ -0,0 +1,70 @@
+package cyoa_handlers
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/preslavmihaylov/learn-golang/gophercises/ex03-cyoa/story"
+)
+
+type cyoaHandler struct {
+	story  *story.Story
+	tmpl   *template.Template
+	prefix string
+}
+
+// CYOAHandlerOpts is a functional option wrapper for the internal cyoaHandler type
+type CYOAHandlerOpts func(h *cyoaHandler)
+
+// NewCYOAHandler constructs a new cyoa handler implementing the http.Handler interface.
+func NewCYOAHandler(st *story.Story, tmpl *template.Template, opts ...CYOAHandlerOpts) http.Handler {
+	h := cyoaHandler{st, tmpl, "/"}
+	for _, opt := range opts {
+		opt(&h)
+	}
+
+	return h
+}
+
+// WithPrefix returns a functional option setting the prefix of the internal cyoa handler type
+func WithPrefix(prefix string) CYOAHandlerOpts {
+	return func(h *cyoaHandler) {
+		h.prefix = prefix
+	}
+}
+
+// ServeHTTP renders a CYOA story.
+// If the provided url path is the handler's prefix (default "/"), the intro chapter will be displayed.
+// If the provided url is anything else, the chapter with the given id will be displayed.
+// In case of an error (chapter not found, can't render template), an error status code is returned
+func (h cyoaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var chapter story.Chapter
+	var err error
+
+	chapterID := trimPrefix(r.URL.Path, h.prefix)
+	if chapterID != "" {
+		chapter, err = h.story.ChapterByID(chapterID)
+	} else {
+		chapter, err = h.story.IntroChapter()
+	}
+
+	if err != nil {
+		http.Error(
+			w, fmt.Sprintf("Failed to find chapter by ID. Received err: %s", err), http.StatusNotFound)
+
+		return
+	}
+
+	err = h.tmpl.Execute(w, chapter)
+	if err != nil {
+		http.Error(
+			w, fmt.Sprintf("Failed to render template: %s", err), http.StatusInternalServerError)
+
+		return
+	}
+}
+
+func trimPrefix(src string, prefix string) string {
+	return src[len(prefix):]
+}
--- gophercises/ex03-cyoa/cmd/web/main.go
+++ gophercises/ex03-cyoa/cmd/web/main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
-	"github.com/preslavmihaylov/learn-golang/gophercises/ex03-cyoa/cmd/web/handlers"
+	"github.com/preslavmihaylov/learn-golang/gophercises/ex03-cyoa/cmd/web/cyoa_handlers"
+	"github.com/preslavmihaylov/learn-golang/gophercises/ex03-cyoa/options"
+	"github.com/preslavmihaylov/learn-golang/gophercises/ex03-cyoa/story"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.CYOAHandler)
+	defaultStory, err := story.FromJSONFile(options.JSONFilename())
+	if err != nil {
+		log.Fatalf("received error while parsing default story: %s", err)
+	}
+
+	defaultTmpl, err := template.ParseFiles(options.HTMLTemplateFilename())
+	if err != nil {
+		log.Fatalf("failed to parse default template. Received err: %s", err)
+	}
+
+	cyoaHandler := cyoa_handlers.NewCYOAHandler(defaultStory, defaultTmpl)
 
 	port := 8080
 	fmt.Printf("Server listening on port %d\n", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), cyoaHandler)
 	if err != nil {
 		log.Fatalf("failed to serve server at %d. Received err: %s", port, err)
 	}
