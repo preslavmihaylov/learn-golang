0cc53e87e6347512712f21428d055ce4b38f7f10
PreslavMihaylov <preslav@pmihaylov.com>
2019-06-01 20:44:37 +0300

    added initial stub commands package

---
+++ gophercises/ex07-task/cmd/task/go.mod
@@ -0,0 +1,3 @@
+module github.com/preslavmihaylov/learn-golang/gophercises/ex07-task/cmd/task
+
+require github.com/spf13/cobra v0.0.4
---
+++ gophercises/ex07-task/cmd/task/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/preslavmihaylov/learn-golang/gophercises/ex07-task/cmd/task/commands"
+
+func main() {
+	commands.Execute()
+}
