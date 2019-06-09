e77c5242dbc4ab6204a193cde353f3e7b35e224f
PreslavMihaylov <preslav@pmihaylov.com>
2019-06-09 22:13:17 +0300

    integrated gorilla/mux for routing

--- go-webdev/lenslocked.com/main.go
+++ go-webdev/lenslocked.com/main.go
@@ -2,30 +2,43 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", rootHandler)
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/contacts", contactsHandler)
+	r.HandleFunc("/faq", faqHandler)
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	fmt.Println("Listening on port 8080...")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "<h1>We could not find the page you were looking for :(</h1>"+
+		"<p>please email us if you keep being sent to an invalid page</p>")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
-	} else if r.URL.Path == "/contacts" {
-		fmt.Fprintf(w, "to get in touch, please send an email to "+
-			"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "<h1>We could not find the page you were looking for :(</h1>"+
-			"<p>please email us if you keep being sent to an invalid page</p>")
-	}
+	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+}
+
+func contactsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "to get in touch, please send an email to "+
+		"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>FAQ</h1>")
 }
