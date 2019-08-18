427d49aeeb9a11cada8e3a54def5940a9706c6a5
PreslavMihaylov <preslav@pmihaylov.com>
2019-08-18 19:37:25 +0300

    added make clean option to services

--- go-micro-tutorial/build.sh
+++ go-micro-tutorial/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-make -C consignment-cli
-make -C consignment-service
-make -C vessel-service
+make -C consignment-cli build
+make -C consignment-service build
+make -C vessel-service build
+
 docker-compose build
+
+make -C consignment-cli clean
+make -C consignment-service clean
+make -C vessel-service clean
