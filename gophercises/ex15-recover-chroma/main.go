d2bbc08c7c692fc5f82bbf03f9fa8f14505983e0
Preslav Mihaylov <pmihay@ext.uber.com>
2019-09-30 09:02:21 +0300

    added initial stub for exercise 15

---
+++ gophercises/ex15-recover-chroma/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"runtime/debug"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/panic/", panicDemo)
+	mux.HandleFunc("/panic-after/", panicAfterDemo)
+	mux.HandleFunc("/", hello)
+	log.Fatal(http.ListenAndServe(":3000", devMw(mux)))
+}
+
+func devMw(app http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println(err)
+				stack := debug.Stack()
+				log.Println(string(stack))
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, string(stack))
+			}
+		}()
+		app.ServeHTTP(w, r)
+	}
+}
+
+func panicDemo(w http.ResponseWriter, r *http.Request) {
+	funcThatPanics()
+}
+
+func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello!</h1>")
+	funcThatPanics()
+}
+
+func funcThatPanics() {
+	panic("Oh no!")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>Hello!</h1>")
+}
