f45187e1d993dbde056ee5fda80bef0bc81daaa1
Preslav Mihaylov <pmihay@ext.uber.com>
2019-10-04 09:55:48 +0300

    implemented gophercise 17

--- gophercises/ex17-secret/main.go
+++ gophercises/ex17-secret/main.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os/user"
 
 	"github.com/preslavmihaylov/learn-golang/gophercises/ex17-secret/secret"
 )
 
 func main() {
-	v, err := secret.FileVault("encoding-key", "secrets")
+	currUsr, err := user.Current()
+	if err != nil {
+		log.Fatalf("failed to get current user: %s", err)
+	}
+
+	cmd := flag.String("c", "", "command you want to execute. [set, get]")
+	key := flag.String("k", "", "key you want to set")
+	val := flag.String("v", "", "value you want to set")
+	pass := flag.String("p", "", "the passphrase of file")
+	inputFilename := flag.String("i", currUsr.HomeDir+"/.secrets_goph", "input file. Defaults to ~/.secrets_goph if not specified")
+	flag.Parse()
+
+	if *cmd == "" {
+		log.Fatalf("command is required")
+	} else if *key == "" {
+		log.Fatalf("key is required")
+	} else if *cmd == "set" && *val == "" {
+		log.Fatalf("value is required")
+	} else if *pass == "" {
+		log.Fatalf("passphrase is required")
+	}
+
+	v, err := secret.FileVault(*pass, *inputFilename)
 	if err != nil {
 		log.Fatalf("failed to create file vault: %s", err)
 	}
 
-	v.Set("key-name", "key-value")
-	v.Set("key-2", "key-2")
+	switch *cmd {
+	case "set":
+		err = v.Set(*key, *val)
+		if err != nil {
+			log.Fatalf("failed to set key: %s", err)
+		}
 
-	value, err := v.Get("key-name")
-	fmt.Println(value) // "key-value"
+		fmt.Println("the key has been saved")
+	case "get":
+		value, err := v.Get(*key)
+		if err != nil {
+			log.Fatalf("failed to get key: %s", err)
+		}
 
-	value, err = v.Get("key-2")
-	fmt.Println(value) // "key-value"
+		fmt.Println(value)
+	}
 }
--- gophercises/ex17-secret/secret/secret.go
+++ gophercises/ex17-secret/secret/secret.go
@@ -2,22 +2,23 @@ package secret
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
 	"encoding/gob"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 )
 
 func FileVault(encKey, filename string) (*Vault, error) {
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		bs, err := mapToBytes(make(map[string]string))
+		err := writeMapToFile(make(map[string]string), filename, encKey)
 		if err != nil {
-			return nil, err
-		}
-
-		err = ioutil.WriteFile(filename, bs, 0666)
-		if err != nil {
-			return nil, fmt.Errorf("failed to write serialized data to file: %s", err)
+			return nil, fmt.Errorf("failed to write map to file: %s", err)
 		}
 	}
 
@@ -30,38 +31,23 @@ type Vault struct {
 }
 
 func (v *Vault) Set(key, value string) error {
-	bs, err := ioutil.ReadFile(v.filename)
-	if err != nil {
-		return fmt.Errorf("failed to read from file: %s", err)
-	}
-
-	store, err := bytesToMap(bs)
+	store, err := getMapFromFile(v.filename, v.encKey)
 	if err != nil {
 		return err
 	}
 
 	store[key] = value
 
-	bs, err = mapToBytes(store)
+	err = writeMapToFile(store, v.filename, v.encKey)
 	if err != nil {
 		return err
-	}
-
-	err = ioutil.WriteFile(v.filename, bs, 0666)
-	if err != nil {
-		return fmt.Errorf("failed to serialize data to file: %s", err)
 	}
 
 	return nil
 }
 
 func (v *Vault) Get(key string) (string, error) {
-	bs, err := ioutil.ReadFile(v.filename)
-	if err != nil {
-		return "", fmt.Errorf("failed to read from file: %s", err)
-	}
-
-	store, err := bytesToMap(bs)
+	store, err := getMapFromFile(v.filename, v.encKey)
 	if err != nil {
 		return "", err
 	}
@@ -72,6 +58,88 @@ func (v *Vault) Get(key string) (string, error) {
 	}
 
 	return val, nil
+}
+
+func getMapFromFile(filename string, key string) (map[string]string, error) {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from file: %s", err)
+	}
+
+	decryptedBs, err := decrypt(bs, key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt data: %s", err)
+	}
+
+	store, err := bytesToMap(decryptedBs)
+	if err != nil {
+		return nil, err
+	}
+
+	return store, nil
+}
+
+func writeMapToFile(store map[string]string, filename string, key string) error {
+	bs, err := mapToBytes(store)
+	if err != nil {
+		return err
+	}
+
+	encryptedBs, err := encrypt(bs, key)
+	if err != nil {
+		return fmt.Errorf("failed to encrypt data: %s", err)
+	}
+
+	err = ioutil.WriteFile(filename, encryptedBs, 0666)
+	if err != nil {
+		return fmt.Errorf("failed to serialize data to file: %s", err)
+	}
+
+	return nil
+}
+
+func encrypt(data []byte, passphrase string) ([]byte, error) {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cipher: %s", err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext, nil
+}
+
+func decrypt(data []byte, passphrase string) ([]byte, error) {
+	key := []byte(createHash(passphrase))
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new cipher: %s", err)
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create GCM cipher: %s", err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decipher text: %s", err)
+	}
+
+	return plaintext, nil
+}
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+
+	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func mapToBytes(mp map[string]string) ([]byte, error) {
